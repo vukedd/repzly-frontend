b2ea1a9ddf52d8fb114e5589a397f46d29a5ea20
vukedd <vukedd@gmail.com>
2025-05-17 23:39:40 +0200

    Made the animation colors adjust to the theme

--- src/app/animated-banner/animated-banner.component.ts
+++ src/app/animated-banner/animated-banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, Renderer2, Input, model } from '@angular/core';
 import { ThemeService } from '../theme.service';
+import { Console } from 'console';
 
 @Component({
   selector: 'app-animated-banner',
@@ -8,49 +9,45 @@ import { ThemeService } from '../theme.service';
 })
 export class AnimatedBannerComponent implements OnInit {
 
-  @Input() color1: string = '#2563EB';
-  @Input() color2: string = '#ffffff';
-  @Input() color3: string = '#000000';
-  @Input() color4: string = '#f0f0f0';
-  @Input() speed: string = '60s'; // e.g., '10s', '20s'
+  colorAccent1: string = '#2563EB';
+  colorAccent2: string = '#f0f0f0';
+  textColor: string = '#F3F4F6';
+  shadowColor: string = 'rgba(0, 0, 0, 0.4)';
 
   constructor(private el: ElementRef, private renderer: Renderer2, private themeService: ThemeService) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.applyDynamicStyles();
-    this.themeService.darkMode$.subscribe(
-      {
-        next: (isDark: boolean) => {
-          if (isDark) {
-            
+  ngAfterViewInit() {
+      setTimeout(() => {
+          this.themeService.darkMode$.subscribe(
+          {
+            next: (isDark: boolean) => {
+              if (isDark) {
+                this.colorAccent1 = '#27272A';
+                this.colorAccent2 = '#27272A';  
+                this.textColor = '#F3F4F6';
+                this.shadowColor =  'rgba(0, 0, 0, 0.4)';
+              } else {
+                this.colorAccent1 = '#E0F2FE';
+                this.colorAccent2 = '#F3F4F6';
+                this.textColor = '#111827';
+                this.shadowColor =  'rgba(0, 0, 0, 0.2)';
+              }
+
+              this.applyDynamicStyles();
+            }
           }
-        }
-      }
-    )
-  }
-
-  ngOnChanges(): void { // If you want to react to @Input changes
-    this.applyDynamicStyles();
+        )
+      });
   }
 
   private applyDynamicStyles(): void {
     const bannerElement = this.el.nativeElement.querySelector('.animated-gradient-banner');
     if (bannerElement) {
-      this.renderer.setStyle(bannerElement, '--gradient-color-1', this.color1);
-      this.renderer.setStyle(bannerElement, '--gradient-color-2', this.color2);
-      this.renderer.setStyle(bannerElement, '--gradient-color-3', this.color3);
-      this.renderer.setStyle(bannerElement, '--gradient-color-4', this.color4);
-      this.renderer.setStyle(bannerElement, '--animation-speed', this.speed);
+      bannerElement.style.setProperty('--gradient-color-accent1', this.colorAccent1);
+      bannerElement.style.setProperty('--gradient-color-accent2', this.colorAccent2);
+      bannerElement.style.setProperty('--text-color', this.textColor);
+      bannerElement.style.setProperty('--text-shadow-color', this.shadowColor);
     }
-  }
-
-  // Example method to change colors programmatically
-  public changeGradient(): void {
-    this.color1 = '#FFC3A0'; // A new set of colors
-    this.color2 = '#FF677D';
-    this.color3 = '#D4A5A5';
-    this.color4 = '#392A48';
-    this.speed = '10s';
-    this.applyDynamicStyles();
   }
 }
